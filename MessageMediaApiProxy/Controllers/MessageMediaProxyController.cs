e680a76994cd084f14276d45154b1dd95f54d04a
Sherwin <sherwinzxw1990@gmail.com>
2021-07-30 20:04:24 +1000

    Updated MessageMediaApiProxy to remove undefined references.

--- MessageMediaApiProxy/Controllers/MessageMediaProxyController.cs
+++ MessageMediaApiProxy/Controllers/MessageMediaProxyController.cs
@@ -12,15 +12,18 @@ namespace MessageMediaApiProxy.Controllers
 {
     public class MessageReceived
     {
+        // the phone number of the sms message destination
         public string PhoneNumber { get; set; }
+
+        // the content of the sms message body
         public string MessageContent { get; set; }
     }
 
 
     public class MessageMediaProxyController : ApiController
     {
-        readonly string API_KEY = "";
-        readonly string API_SECRET = "";
+        readonly string API_KEY = "";       // replace with the MessageMedia API Key
+        readonly string API_SECRET = "";    // replace with the MessageMedia API Secret
         //readonly string API_KEY = SystemSettingsController.GetSystemSetting("CAMP", "MessageMediaApiKey");
         //readonly string API_SECRET = SystemSettingsController.GetSystemSetting("CAMP", "MessageMediaApiSecreat");
         readonly Boolean HMAC = false;
@@ -33,13 +36,15 @@ namespace MessageMediaApiProxy.Controllers
 
             MessagesController messages = client.Messages;
 
-            string SMSContent = "";
+            string SMSContent = "";     // replace with the sms message content template
             //string SMSContent = SMSMessagesController.GetSMSMessageContent(smsMessageCode);
             SMSContent = SMSContent.Replace("{SystemDateTime}", DateTime.Now.ToString());
 
             SendMessagesRequest body = new SendMessagesRequest();
             body.Messages = new List<Message>();
 
+            
+            // loop through the phoneNumbers list to populate the Messages object
             for (int i = 0; i < phoneNumbers.Count; i++)
             {
                 Message body_messages = new Message();
@@ -47,8 +52,10 @@ namespace MessageMediaApiProxy.Controllers
                 body_messages.DestinationNumber = phoneNumbers[i];
                 body.Messages.Add(body_messages);
             }
+
             try
             {
+                // use the MessageMedia VS Extension to call the MessageMedia API POST method to send messages
                 SendMessagesResponse result = messages.SendMessagesAsync(body).Result;
                 return "Messages successfully sent.";
             }
